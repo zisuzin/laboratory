cd8c75199ef89566f92c31a0510d3cb642f277e0
zisuzin <zi_soo_zin@naver.com>
2023-05-25 23:34:00 +0900

    ㄴ

---
+++ react/03.컴포넌트/App.js
@@ -0,0 +1,35 @@
+function Header(){ // 사용자정의태그는 대문자로 시작!
+    return <header>
+        <h1>
+            <a href="#">WEB</a>
+        </h1>
+    </header>
+}
+
+function App() { 
+    return (
+        <div>
+            <Header></Header>
+            <nav>
+            <ul>
+                <li>
+                    <a href="#">HTML</a>
+                </li>
+                <li>
+                    <a href="#">CSS</a>
+                </li>
+                <li>
+                    <a href="#">JavaScript</a>
+                </li>
+            </ul>
+                </nav>
+                <article>
+            <h2>HTML</h2>
+            HTML is HyperText Markup Language
+                </article>
+        </div>
+    )
+        
+}
+
+export default App;
